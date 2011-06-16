58fab7d6e15ba3ec3c5a461e3789f35f154b359b
edwindjonge <edwindjonge@cb06e305-f834-26d0-b1f6-d1fa8a6c78bd>
2011-06-16 06:54:29 +0000

    updated batchscript
    git-svn-id: https://fffunctions.googlecode.com/svn/trunk@59 cb06e305-f834-26d0-b1f6-d1fa8a6c78bd

--- build.bat
+++ build.bat
@@ -5,9 +5,10 @@ echo Removing building information...
 rm -rf output
 
 echo Generate documentation...
-R -q -f roxygen.R
+Rscript roxygen.R
 
 md output
 cd output
 R CMD build ../pkg
 FOR %%1 in (*.tar.gz) DO R CMD check %%1
+cd ..
