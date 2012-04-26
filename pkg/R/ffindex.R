5035fe07bd847bf4b5c821e5dcd8b000ed2b8062
Edwin de Jonge <edwindjonge@gmail.com>
2012-04-26 22:46:09 +0200

    added ffindex

--- pkg/R/ffindex.R
+++ pkg/R/ffindex.R
@@ -1,12 +1,15 @@
 #' Add a ff vector that contains the order of the \code{\link{ff}} vector \code{x}
 #' 
-#' The index can be retrieve using \code{ffindex}. Note that you have to assign the result to the
-#' Original vector \code{x}.
+#' Add a ff vector that contains the order of the \code{\link{ff}} vector \code{x}
+#' The index can be retrieved using \code{ffindex}. Note that you have to assign the result to the
+#' original vector \code{x}.
+#' @example ../examples/ffindex.R
 #' @param x \code{ff} vector to be indexed
-#' @param addKey should the sorted values also be stored in.
+#' @param addKey should the sorted values also be stored in \code{ffkey}?
 #' @param ... parameters that will be passed on to \code{\link{fforder}}. 
 #' @return The updated vector \code{x}
 #' @rdname ffindex
+#' @export addffIndex ffindex ffkey
 addffIndex <- function(x, addKey=FALSE, ...){
   index <- fforder(x)
   filename(index) <- sub("(\\.ff)?$", ".ffindex", filename(x))
--- pkg/R/mean.R
+++ pkg/R/mean.R
@@ -7,6 +7,7 @@
 #' @param trim percentage of robustness, between 0 and 1
 #' @param ... other arguments passed to \code{mean}
 #' @param range a \code{ri} or an \code{integer} vector of \code{length==2} giving a range restriction for chunked processing
+#' @importFrom stats weighted.mean
 #' @return mean value
 mean.ff <- function(x, trim=0, ..., range=NULL){
     r <- checkRange(range, x)
@@ -18,7 +19,7 @@ mean.ff <- function(x, trim=0, ..., range=NULL){
 	   n <- length(x)     #calculate length
 	   r[1] <- floor(n * trim)
 	   r[2] <- n - r[1]
-	}
+	  }
 	
    
    res <- sapply( chunk(x, from=min(r), to=max(r))
@@ -28,17 +29,5 @@ mean.ff <- function(x, trim=0, ..., range=NULL){
                       )
                   }
 				    )
-   stats::weighted.mean(res['mean',], res['w',])
-}
-
-#' Mean of ffdf vector
-#' 
-#' @method mean ffdf
-#' @export
-#' @param x a \code{ffdf}
-#' @param ... other arguments passed to \code{\link{mean.ff}}
-#' @param range a \code{ri} or an \code{integer} vector of \code{length==2} giving a range restriction for chunked processing
-#' @return a vector with the mean values
-mean.ffdf <- function(x, ..., range=NULL){
-   sapply(physical(x), mean, ..., range=range)
+   weighted.mean(res['mean',], res['w',])
 }
