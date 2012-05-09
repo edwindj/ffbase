f78041327211669bfec91492246c3673525b9d08
Jan Wijffels <jwijffels@bnosac.be>
2012-05-09 19:17:30 +0200

    Allowed ffmatch for ffdf

--- examples/match.R
+++ examples/match.R
@@ -3,8 +3,15 @@ x.ff <- ffmatch(as.ff(as.factor(c(LETTERS, NA))), as.ff(as.factor(c("C","B","Z",
 class(x.ff)
 x <- match(c(LETTERS, NA), c("C","B","Z","X","HMM","Nothing",NA))
 table(x.ff[] == x, exclude=c())
+## ffmatch also allows to input an ffdf
+data(iris)
+ffiris <- as.ffdf(iris)
+ffirissubset <- as.ffdf(iris[c(1:10, nrow(iris)), ])
+ffmatch(ffiris, ffirissubset, trace=TRUE, BATCHBYTES=500)
 ## %in% is masked from the base package 
 as.ff(as.factor(c(LETTERS, NA))) %in% as.ff(as.factor(c("C","B","Z","X","HMM","Nothing",NA)))
 as.factor(c(LETTERS, NA)) %in% as.factor(c("C","B","Z","X","HMM","Nothing",NA))
+ffiris %in% ffirissubset
+
 
 
--- pkg/R/match.R
+++ pkg/R/match.R
@@ -1,11 +1,15 @@
 #' Value Matching for ff objects
 #'
 #' \code{match} returns an ff vector of the positions of (first) matches of its first argument in its second. 
-#' Similar as \code{\link[base]{match}}.
+#' Similar as \code{\link[base]{match}}. \cr
+#' ffmatch also allows to match ffdf objects by \code{past}ing together the columns of the ffdf and matching on the pasted column.\cr \cr
+#' \code{\%in\%} returns a logical ff vector indicating if there is a match or not for its left operand. 
+#' ffdf objects are also allowed in the left and right operand of the \code{\%in\%} operator. See the examples.
 #'
 #' @export
-#' @param x a \code{ff} object
-#' @param table a \code{ff} object
+#' @example ../examples/match.R
+#' @param x a \code{ff} object or an \code{ffdf} object
+#' @param table a \code{ff} object or an \code{ffdf} object
 #' @param nomatch the value to be returned in the case when no match is found. Note that it is coerced to \code{integer}.
 #' @param incomparables a vector of values that cannot be matched. Any value in \code{x} matching a value in this vector is assigned the nomatch value. For historical reasons, \code{FALSE} is equivalent to \code{NULL}.
 #' @param trace logical indicating to show on which chunk the function is computing
@@ -13,6 +17,9 @@
 #' @return An ff vector of the same length as \code{x}. An integer vector giving the position in table of the first match if there is a match, otherwise \code{nomatch}. 
 #' @seealso \code{\link[base]{match}}
 ffmatch <- function(x, table, nomatch = NA_integer_, incomparables = NULL, trace=FALSE, ...){
+	stopifnot(any(c(inherits(x, "ff_vector"), inherits(table, "ff_vector"), inherits(x, "ffdf"), inherits(table, "ffdf"))))
+	stopifnot(inherits(x, "ff_vector") == inherits(table, "ff_vector"), inherits(x, "ffdf") == inherits(table, "ffdf"))
+	
   nomatch <- as.integer(nomatch)
   xchunk <- chunk(x, ...)
   tablechunk <- chunk(table, ...)
@@ -22,9 +29,12 @@ ffmatch <- function(x, table, nomatch = NA_integer_, incomparables = NULL, trace
   }
   res <- NULL
   ## First work on looping over x, then over the table
-  for (i in xchunk){
-    
-    xi <- x[i]
+  for (i in xchunk){    
+    if(inherits(x, "ffdf")){
+    	xi <- x[i, ]
+    }else{
+    	xi <- x[i]
+    }    
     unmatched <- TRUE
     
     if (trace){
@@ -36,7 +46,11 @@ ffmatch <- function(x, table, nomatch = NA_integer_, incomparables = NULL, trace
       if(trace) {
         message(sprintf("%s, working on table chunk %s:%s", Sys.time(), min(j), max(j)))      
       }
-      m[unmatched] <- match(x=xi[unmatched], table=table[j], incomparables=incomparables) +  min(j) - 1L
+      if(inherits(x, "ffdf")){
+    		m[unmatched] <- match(x=do.call(paste, xi[unmatched, ]), table=do.call(paste, table[j, ]), incomparables=incomparables) +  min(j) - 1L
+    	}else{
+    		m[unmatched] <- match(x=xi[unmatched], table=table[j], incomparables=incomparables) +  min(j) - 1L
+    	}       
       unmatched <- is.na(m)
       if (!any(unmatched)) break
     }
@@ -46,13 +60,18 @@ ffmatch <- function(x, table, nomatch = NA_integer_, incomparables = NULL, trace
   res
 }
 
+
+in.default <- get(x="%in%")
 #' @rdname ffmatch
 #' @export
+#' @usage x \%in\% table
 "%in%" <- function(x, table){
 	if(inherits(x, "ff_vector") & inherits(table, "ff_vector")){
-		ffmatch(x=x, table=table, nomatch = 0) > 0
+		ffmatch(x=x, table=table, nomatch = 0L) > 0L
+	}else if(inherits(x, "ffdf") & inherits(table, "ffdf")){
+		ffmatch(x=x, table=table, nomatch = 0L) > 0L
 	}else{
-		match(x=x, table=table, nomatch = 0) > 0
+		in.default(x=x, table=table)
 	}	
 }
 
