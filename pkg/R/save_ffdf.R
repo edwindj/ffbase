333396d1ece7870fba22d6cf47a19a2a18e69ace
Edwin de Jonge <edwindjonge@gmail.com>
2012-06-01 16:10:15 +0200

    GSM build

--- pkg/R/save_ffdf.R
+++ pkg/R/save_ffdf.R
@@ -7,10 +7,11 @@
 #' @example ../examples/save_ffdf.R
 #' @param ... \code{ffdf} data.frames, \code{ff} vectors, or other variables to be saved in the directory
 #' @param dir  path where .rdata file will be saved and all columns of supplied \code{ffdf}'s. It will be created if it doesn't exist.
-#' @param relativepath \code{logical} if \code{TRUE} the ff vectors will have relative paths, making the copying the directory a simple
+#' @param clone should the data.frame be cloned?
+#' @param relativepath \code{logical} if \code{TRUE} the stored ff vectors will have relative paths, making moving the data to another storage a simple
 #' copy operation. 
 #' @export
-save.ffdf <- function(..., dir="./db", relativepath=TRUE){
+save.ffdf <- function(..., dir="./db", clone=FALSE, relativepath=clone){
   
    names <- as.character(substitute(list(...)))[-1L]
    dir.create(dir, showWarnings=FALSE, recursive=TRUE)
@@ -19,8 +20,11 @@ save.ffdf <- function(..., dir="./db", relativepath=TRUE){
    on.exit(setwd(oldwd))
    
    for (n in names){
-     x = get(n)
+     x = get(n, pos=1)
      if (is.ffdf(x)) {
+       if (isTRUE(clone)){
+         x <- clone(x)
+       }
        assign(n, move.ffdf(x, dir=".", name=n, relativepath=relativepath))
      }
    }
@@ -67,20 +71,68 @@ move.ffdf <- function(x, dir=".", name=as.character(substitute(x)), relativepath
   x
 }
 
-pack <- function(zipfile, ...){
-  td <- tempdir()
-  save.ffdf(..., dir=td)
+load.ffdf <- function(dir, envir=parent.frame()){
+  oldwd <- setwd(dir)
+  on.exit(setwd(oldwd))
+  
+  env <- new.env()
+  
+  load(".RData", envir=env)
+  names <- ls(envir=env, all.names=TRUE)
+  
+  for (n in names){
+    x = get(n, envir=env)
+    if (is.ffdf(x)){
+      for (i in physical(x)){
+        filename(i) <- filename(i)
+      }
+      close(x)
+    } else if (is.ff(x)){
+      filename(x) <- filename(x)
+      close(x)
+    }
+    assign(n, x, envir=envir)
+  }
+  invisible(env)
+}
+
+pack.ffdf <- function(file, ...){
+  td <- tempfile("pack")
+  save.ffdf(..., dir=td, clone=TRUE, relativepath=TRUE)
+  
+  file.create(file)
+  file <- file_path_as_absolute(file)
+  file.remove(file)
   
   oldwd <- setwd(td)
   on.exit(setwd(oldwd))
   
-  d <- c(".Rprofile", ".RData", dir(td, recursive=FALSE))
-  zip(zipfile, d, )
+  d <- c(".Rprofile", ".RData", dir(td))
+  
+  # if file extension is zip, zip it otherwise tar.gz it
+  switch( file_ext(file)
+        , zip = zip(zipfile=file, files=d)
+        , tar(tarfile=file, ".", compression="gzip")
+        )
+}
+
+unpack.ffdf <- function(file, dir=NULL, envir=parent.frame()){
+  if (is.null(dir)){ 
+    dir <- tempfile("unpack")
+  }
+  
+  switch( file_ext(file)
+        , zip = unzip(zipfile=file, exdir=dir)
+        , untar(tarfile=file, exdir=dir)
+  )
+  
+  env <- load.ffdf(dir, envir=envir)
+  invisible(env)
 }
 
 first <- function(){
   
-  if (!require(ff)){
+  if (!require(ffbase)){
     stop("Please install package ff, otherwise the files cannot be loaded.")
   }
   
@@ -99,6 +151,4 @@ first <- function(){
 }
 
 # x <- as.ffdf(iris)
-# x1 <- move.ffdf(x, dir="./db3")
-# 
-# save.ffdf(x, dir="./db5")
+# pack.ffdf("test.zip", x)
