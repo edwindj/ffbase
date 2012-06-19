f55074b83d8eeb2c770d8050d5792b7ca40b2f20
Edwin de Jonge <edwindjonge@gmail.com>
2012-06-19 12:58:56 +0200

    Added bySum and bySumsq, aggregate style

---
+++ pkg/src/bySum.c
@@ -0,0 +1,34 @@
+#include <R.h>
+#include <Rdefines.h>
+#include <R_ext/Error.h>
+
+SEXP bySum ( SEXP x, SEXP by, SEXP nbins, SEXP weight) {
+
+     double *rx = REAL(x);
+     int *ibin = INTEGER(by);
+     int nb = *INTEGER(nbins);
+     int nx = length(x);
+     double *w = REAL(weight);
+    
+     SEXP res;
+     PROTECT(res=allocMatrix(REALSXP, nb, 3));
+     double *rres = REAL(res);
+    
+	   for (int i = 0; i < 3*nb; i++){
+         rres[i] = 0;
+     }
+                  
+     for (int i = 0; i < nx; i++){
+        int b = ibin[i] - 1;
+        if (-1 < b && b < nb){
+           if (ISNA(rx[i])){
+             rres[b + 2*nb] += w[i];
+           } else {
+             rres[b] += w[i];
+             rres[b + nb] += rx[i];
+           }
+        }
+     }
+     UNPROTECT(1);
+     return res;
+}
---
+++ pkg/src/bySumsq.c
@@ -0,0 +1,35 @@
+#include <R.h>
+#include <Rdefines.h>
+#include <R_ext/Error.h>
+
+SEXP bySumsq ( SEXP x, SEXP mean, SEXP by, SEXP nbins, SEXP weight) {
+     double *rx = REAL(x);
+     double *rmean = REAL(mean);
+     double *w = REAL(weight);
+                            
+     int *ibin = INTEGER(by);
+     int nb = *INTEGER(nbins);
+     int nx = length(x);
+    
+     SEXP res;
+     PROTECT(res=allocMatrix(REALSXP, nb, 2));
+     double *rres = REAL(res);
+     for (int i = 0; i < 3*nb; i++){
+         rres[i] = 0;
+     }
+                  
+     for (int i = 0; i < nx; i++){
+        int b = ibin[i] - 1;
+        double val = rx[i] - rmean[i];
+		    if (-1 < b && b < nb){
+           if (!ISNA(val)){
+             rres[b + 2*nb] += w[i];
+           } else {
+             rres[b] += w[i];
+             rres[b + nb] += val*val;
+           }
+        }
+     }
+     UNPROTECT(1);
+     return res;
+}
