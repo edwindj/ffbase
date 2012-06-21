53559d5f8779531138b29a166f7bebd706c915ae
Edwin de Jonge <edwindjonge@gmail.com>
2012-06-21 10:33:11 +0200

    Added byMean

--- pkg/R/byMean.R
+++ pkg/R/byMean.R
@@ -1,4 +1,4 @@
-bySum <- function(x, by, na.rm=FALSE, weight=NULL...){
+byMean <- function(x, by, na.rm=FALSE, weight=NULL...){
   
   if (!is.list(by)){
     index <- list(by)
@@ -24,7 +24,8 @@ bySum <- function(x, by, na.rm=FALSE, weight=NULL...){
     bs <- .Call("bySum", as.numeric(x), as.integer(bin), as.integer(nbins), as.numeric(weight), PACKAGE = "ffbase")    
   }
   
-  res <- bs[,2]
+  res <- bs[,2] / bs[,1]
+  is.na(res) <- bs[,1] == 0
   
   if (!na.rm){
     is.na(res) <- (bs[,3] > 0)
@@ -39,22 +40,23 @@ bySum <- function(x, by, na.rm=FALSE, weight=NULL...){
 # x <- as.numeric(1:100000)
 # bin <- as.factor(as.integer(runif(length(x), 1, 101)))
 # x[1] <- NA
-# weight <- rep(1, length(x))
+# weight <- runif(length(x))
 # 
-# bySum(1:10, factor(1:10), weight=10:1)
+# byMean(1:10, factor(1:10), weight=10:1)
 # 
-# bySum(x, bin, na.rm=TRUE)
+# byMean(x, bin, na.rm=TRUE)
 # 
 # system.time({
-#   replicate(50, {tapply(x, bin, function(i){c(sum=sum(i, na.rm=TRUE), na=sum(is.na(i)))})})
+#   replicate(50, {tapply(x, bin, function(i){c(mean=mean(i, na.rm=TRUE), na=sum(is.na(i)))})})
+# })
+# 
+# 
+# system.time({
+#   replicate(50, {byMean(x, bin, na.rm=TRUE)})
 # })
 # 
 # system.time({
-#   replicate(50, {bySum(x, bin, na.rm=TRUE)})
-# })
-# 
-# system.time({
-#   replicate(50, {bySum(x, bin, na.rm=TRUE, weight=weight)})
+#   replicate(50, {byMean(x, bin, na.rm=TRUE, weight=weight)})
 # })
 # 
 # system.time({
