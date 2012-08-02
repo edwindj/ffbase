f984dcd562e088e3af4a85e5c3e0cf6d20fe6204
Jan Wijffels <jwijffels@bnosac.be>
2012-08-02 22:58:47 +0200

    Small doc change for key

--- pkg/R/key.R
+++ pkg/R/key.R
@@ -7,7 +7,7 @@
 #' @example ../examples/key.R
 #' @param x an \code{ffdf}
 #' @param ... other parameters passed on to chunk
-#' @return An integer ff vector of the same length as the number of rows in \code{x} with unique values for 
+#' @return An integer ff vector of the same length as the number of rows in \code{x} with unique values for each unique row
 key <- function(x, ...){
 	o <- ffdforder(x, na.last = TRUE, decreasing = FALSE)
 	xo <- x[o, , drop=FALSE]
