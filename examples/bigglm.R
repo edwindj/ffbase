333f95e9149e48b81de011cf1e868b289127501c
Jan Wijffels <jwijffels@bnosac.be>
2012-11-09 12:55:18 +0100

    Added exxample of bigglm.ffdf

---
+++ examples/bigglm.R
@@ -0,0 +1,11 @@
+require(biglm)
+data(trees)
+x <- as.ffdf(trees)
+a <- bigglm(log(Volume)~log(Girth)+log(Height), 
+            data=x, chunksize=10, sandwich=TRUE)
+summary(a)
+
+b <- bigglm(log(Volume)~log(Girth)+log(Height)+offset(2*log(Girth)+log(Height)),
+            data=x, chunksize=10, sandwich=TRUE)
+summary(b)
+
--- pkg/R/bigglm.R
+++ pkg/R/bigglm.R
@@ -1,16 +1,18 @@
 #' Bounded memory linear regression
 #'
 #' bigglm.ffdf creates a generalized linear model object that uses only p^2 memory for p variables.
-#' It uses the biglm package and allows to supply an ffdf. Make sure that package is loaded.
+#' It uses the biglm package and is a simple wrapper to allow to work with an ffdf as input data. 
+#' Make sure that package is loaded.
 #'
 #' @param formula a model formula
 #' @param data an object of class ffdf
 #' @param family A glm family object
 #' @param ... other parameters passed on to bigglm. See the bigglm package: \code{\link[biglm]{bigglm}}
 #' @param chunksize Size of chunks for processnig the ffdf
-#' @return An object of class bigglm. . See the bigglm package for a description: \code{\link[biglm]{bigglm}}
+#' @return An object of class bigglm. See the bigglm package for a description: \code{\link[biglm]{bigglm}}
 #' @export 
 #' @seealso \code{\link[biglm]{bigglm}}
+#' @example ../examples/bigglm.R
 bigglm.ffdf<-function(formula, data, family = gaussian(), ..., chunksize=5000){
   if (!require(biglm)){
     stop("This function needs the package 'biglm', which can be installed from CRAN")
@@ -39,20 +41,20 @@ bigglm.ffdf<-function(formula, data, family = gaussian(), ..., chunksize=5000){
   }
   rval<-bigglm(formula, data=chunk, family=family, ...)
   rval$call<-sys.call()
-  rval$call[[1]]<-as.name(.Generic)
+  #rval$call[[1]]<-as.name(.Generic)
+  rval$call[[1]]<-as.name("bigglm")
   rval
 }
 
 
 
 ##### quick testing code ######
-# download.file("http://faculty.washington.edu/tlumley/NO2.dat", "NO2.dat")
 # require(ff)
 # require(biglm)
+# download.file("http://faculty.washington.edu/tlumley/NO2.dat", "NO2.dat")
 # airpoll <- read.table.ffdf(file="NO2.dat")
-# for(i in 1:10) x <- read.table.ffdf(x = airpoll, file = "NO2.dat")
 # colnames(airpoll) <- c("logno2","logcars","temp","windsp","tempgrad","winddir","hour","day")
 # dim(airpoll)
-# b <- bigglm(exp(logno2) ~ logcars + temp + windsp, data=airpoll, family=Gamma(log), start=c(2,0,0,0), 
-#             maxit=100, chunksize=100)
-# summary(b)
+# c <- bigglm(exp(logno2) ~ logcars + temp + windsp, data=airpoll, family=Gamma(log), start=c(2,0,0,0), 
+#             maxit=100, chunksize=10)
+# summary(c)
