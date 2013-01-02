d19a3b659a81acb96d649d330095224c634efc6f
Jan Wijffels <jwijffels@bnosac.be>
2013-01-02 22:02:10 +0100

    Added ffseq

--- examples/ffseq.R
+++ examples/ffseq.R
@@ -1,3 +1,16 @@
+## ffseq_len
 ffseq_len(1000)
 ffseq_len(1000000)
+
+## ffseq
+ffseq(from = 1, to = 4, by = 1)
+ffseq(from = 1, to = 4, by = 0.5)
+ffseq(from = 4, to = 1, by = -0.5)
+ffseq(from = -100, to = 100, by = 0.3)
+ffseq(from = 2, to = -100, length.out = 4)
+ffseq(from = 2, to = -100, length.out = 4)
+ffseq(from = 2, along.with=1000)
+ffseq(to = 2, along.with=1000)
+ffseq(along.with=1000)
+ffseq(length.out=1000000)
 
