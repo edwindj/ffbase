52fb920d4e167b536ab151094d90cb9948a43943
Edwin de Jonge <edwindjonge@gmail.com>
2013-07-01 15:24:02 +0200

    commented as.big.matrix, since it is not on CRAN anymore.

--- pkg/R/as.R
+++ pkg/R/as.R
@@ -21,8 +21,6 @@ as.character.ff <- function(x, ...){
 	res		
 }
 
-
-
 as.ff_matrix <- function(x, ...){
   UseMethod("as.ff_matrix")
 }
@@ -36,41 +34,41 @@ as.ff_matrix.ffdf <- function(x, ...){
   result
 }
 
-
-as.big.matrix.ffdf <- function(x, separated=FALSE, backingfile=NULL, backingpath=NULL, descriptorfile=NULL, shared=TRUE){
-  maxvmode <- maxffmode(vmode(x))
-  type <- switch(maxvmode,
-                 boolean = "short", logical = "short", quad = "short", nibble = "short",
-                 byte = "short", ubyte = "short", short = "short", 
-                 ushort = "integer", integer = "integer",
-                 single = "double", double = "double",
-                 complex = "notyetimplemented",
-                 raw = "notyetimplemented",
-                 character = "notyetimplemented",
-                 "notyetimplemented"              
-  )
-  stopifnot(type %in% c("char","short","integer","double"))
-  containsfactors <- sapply(physical(x), is.factor.ff)
-  if(sum(containsfactors) > 0){
-    warning("factors codes are inserted in big.matrix, not the factor levels")
-    containsfactors <- names(containsfactors)[containsfactors == TRUE]
-    for(column in containsfactors){
-      levels(x[[column]]) <- NULL
-    }
-  }
-  ## Create the big matrix
-  y <- big.matrix(nrow=nrow(x), 
-                  ncol=ncol(x), type=type, 
-                  init=NULL, dimnames=dimnames(x), 
-                  separated=separated,
-                  backingfile=backingfile, 
-                  backingpath=backingpath,
-                  descriptorfile=descriptorfile, 
-                  shared=shared)
-  ## And fill it up
-  for(i in chunk(x)){
-    idx <- as.integer(as.hi(i))              
-    y[idx, ] <- as.matrix(ffbase:::ffdfget_columnwise(x, index=idx))
-  }
-  return(y)
-}
+# 
+# as.big.matrix.ffdf <- function(x, separated=FALSE, backingfile=NULL, backingpath=NULL, descriptorfile=NULL, shared=TRUE){
+#   maxvmode <- maxffmode(vmode(x))
+#   type <- switch(maxvmode,
+#                  boolean = "short", logical = "short", quad = "short", nibble = "short",
+#                  byte = "short", ubyte = "short", short = "short", 
+#                  ushort = "integer", integer = "integer",
+#                  single = "double", double = "double",
+#                  complex = "notyetimplemented",
+#                  raw = "notyetimplemented",
+#                  character = "notyetimplemented",
+#                  "notyetimplemented"              
+#   )
+#   stopifnot(type %in% c("char","short","integer","double"))
+#   containsfactors <- sapply(physical(x), is.factor.ff)
+#   if(sum(containsfactors) > 0){
+#     warning("factors codes are inserted in big.matrix, not the factor levels")
+#     containsfactors <- names(containsfactors)[containsfactors == TRUE]
+#     for(column in containsfactors){
+#       levels(x[[column]]) <- NULL
+#     }
+#   }
+#   ## Create the big matrix
+#   y <- big.matrix(nrow=nrow(x), 
+#                   ncol=ncol(x), type=type, 
+#                   init=NULL, dimnames=dimnames(x), 
+#                   separated=separated,
+#                   backingfile=backingfile, 
+#                   backingpath=backingpath,
+#                   descriptorfile=descriptorfile, 
+#                   shared=shared)
+#   ## And fill it up
+#   for(i in chunk(x)){
+#     idx <- as.integer(as.hi(i))              
+#     y[idx, ] <- as.matrix(ffbase:::ffdfget_columnwise(x, index=idx))
+#   }
+#   return(y)
+# }
